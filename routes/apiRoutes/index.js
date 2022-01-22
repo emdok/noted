622aab3938036b00a78721fbbb60e5ccd31c9d48
Emily Dokken <dokkenemily@gmail.com>
2022-01-21 22:28:18 -0800

    add fs to index.js to attempt to delete file from db

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const { createNote } = require('../../lib/notes');
+const fs = require('fs');
+const path = require('path');
+const { createNote, findById } = require('../../lib/notes');
 const { notes } = require("../../db/db.json");
 var uniqid = require('uniqid'); 
 
@@ -11,13 +13,25 @@ router.get("/notes", (req, res) => {
 router.post("/notes", (req, res) => {
     const noteId = uniqid().toString();
     req.body.id = noteId;
-    
+
     const note = createNote(req.body, notes);
     res.json(note);
 });
 
-router.delete("/notes", (req, res) => {
-    // TODO: Add delete route functionality here
+router.delete("/notes/:id", (req, res) => {
+    console.log("delete route called")
+    const noteToRemove = findById(req.params.id, notes);
+
+    const result = notes.filter(note => note !== noteToRemove)
+    
+    console.log(result);
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify({ notes: result }, null, 2)
+    );
 });
 
 module.exports = router;
+
+/*DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.*/

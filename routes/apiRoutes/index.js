b200d2bb3fdd22c1079da2387250e8410ac9927a
Emily Dokken <dokkenemily@gmail.com>
2022-01-23 14:31:24 -0800

    update post route to grab user input

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -13,24 +13,31 @@ router.get("/notes", (req, res) => {
 });
 
 router.post("/notes", (req, res) => {
-  const note = createNote(req.body, notes);
-  res.json(note);
+
+    if (req.body.title.length > 0 && req.body.text.length > 0) {
+        const note = createNote(req.body, notes);
+        res.json(note);
+    } else {
+        res.status(204).json("Response does not contain body")
+    }
+
 });
 
 router.delete("/notes/:id", (req, res) => {
-  console.log("delete route called");
-  notes = findById(req.params.id, notes);
-
-  console.log(notes);
-
-  fs.writeFileSync("./db/db.json",
-    JSON.stringify({ notes }, null, 2)
-  );
-
-  res.json(notes);
-
+    if (req.params.id.length > 0) {
+        console.log("delete route called");
+        notes = findById(req.params.id, notes);
+      
+        console.log(notes);
+      
+        fs.writeFileSync("./db/db.json",
+          JSON.stringify({ notes }, null, 2)
+        );
+      
+        res.json(notes);
+    } else {
+        res.status(204).json("Response does not contain body")
+    }
 });
 
 module.exports = router;
-
-/*DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.*/
